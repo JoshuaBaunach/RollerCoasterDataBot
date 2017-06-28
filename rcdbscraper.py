2f94bbc4d4c8d1d36d83c097e4955bf08e6f06e7
JoshuaBaunach <joshua.baunach@gmail.com>
2017-06-28 11:52:41 -0400

    Merged rcdbscraper.py and rollercoaster.py into one file

--- rcdbscraper.py
+++ rcdbscraper.py
@@ -1,9 +1,24 @@
 #!/usr/bin/python
 # This script contains functions that handle the second phase of the RollerCoasterDataBot process.
 
-from rollercoaster import RollerCoaster
 from lxml import html
 import requests
+
+# This class defines a roller coaster object and various pieces of info about it
+class RollerCoaster:
+
+    # Constructor (takes in basic information about the coaster that all RCDB articles should have.)
+    def __init__(self, rcdbid, name, park, loc1, loc2, loc3, status, manufacturer, model1, model2):
+        self.rcdbid = rcdbid
+        self.name = name
+        self.park = park
+        self.loc1 = loc1
+        self.loc2 = loc2
+        self.loc3 = loc3
+        self.status = status
+        self.manufacturer = manufacturer
+        self.model1 = model1
+        self.model2 = model2
 
 class ScraperObject:
 
